79ea8c10f18f45facab15dc928e652dec5f0916f
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2022-01-12 20:50:29 +0200

    new

---
+++ 112.1/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _112._1
+{
+    static class Circle
+    {
+        private static readonly double pi = 3.1416;
+        static public double Perimeter(double R)
+        {
+            return 2 * R * pi;
+        }
+
+        static public double Face(double R)
+        {
+            return pi * R * R;
+        }
+    }
+
+    static class Sphere
+    {
+        private static readonly double pi = 3.1416;
+        static public double Surface(double R)
+        {
+            return 4 * pi * R * R;
+        }
+        static public double Volume(double R)
+        {
+            return 4 * pi * R * R * R / 3;
+        }
+    }
+}
---
+++ 112.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _112._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Circle.Perimeter(2));
+            Console.WriteLine(Circle.Face(2));
+            Console.WriteLine(Sphere.Surface(2));
+            Console.WriteLine(Sphere.Volume(2));
+        }
+    }
+}
